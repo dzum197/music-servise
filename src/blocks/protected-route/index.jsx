c2d5b04b2cef6eb35939f3b98a582161708b8bef
Liya <mus.limova_liya@mail.ru>
2023-07-27 19:03:32 +0300

    add new route

--- src/blocks/protected-route/index.jsx
+++ src/blocks/protected-route/index.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable import/prefer-default-export */
-import { Navigate, Outlet } from "react-router-dom";
+// import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export function ProtectedRoute({ redirectPart = '/' }) {
     const login = localStorage.getItem('login');
