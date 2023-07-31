b73b87e62209c33907488be1ac3e63631e4b5e19
Liya <mus.limova_liya@mail.ru>
2023-07-31 20:17:24 +0300

    delete double nav files

--- src/pages/NotFound/NotFound.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./notfound.module.css";
-
-function NotFound() {
-    return (
-        <h2 className={styles.notfound__title}>Страница не найдена</h2>
-    );
-}
-
-export default NotFound;
